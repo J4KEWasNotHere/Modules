17937e52fcc11e02283f5d2fb34f2832a3e0fee9
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-04-04 13:53:05 +0200

    Refactor Debounce class to use ClassV2 type

--- Resources/Debouncer/DebouncerV2.lua
+++ Resources/Debouncer/DebouncerV2.lua
@@ -6,7 +6,6 @@ local Types = require("@self/Types")
 local Debounce = {}
 Debounce.__index = Debounce
 
-local Types = require(script.Types)
 
 function Debounce.new()
 	local self = {_states = {}}
@@ -14,46 +13,42 @@ function Debounce.new()
 end
 
 -- Runs a function if the key isn't in an active debounce
-function Debounce.Run(self: Types.Class, key: string, delayTime: number, fn: () -> ())
+function Debounce.Run(self: Types.ClassV2, key: string, delayTime: number, fn: () -> ())
 	if self:IsActive(key) then
-    local data = self._states[key]
-    if (os.clock() - data[1]) > data[2] then
-      
-    else
-    	return false
-    end
+		return false
 	end
 	
-	self._states[key] = {os.clock(), delayTime}
-
+	self._states[key] = {
+		time = os.clock(),
+		duration = delayTime
+	}
+	
 	fn()
 	return true
 end
 
 -- Checks if a debounce is active for the given key
-function Debounce.IsActive(self: Types.Class, key: string)
-   local data = self._states[key]
-   
-   if (os.clock() - data[1]) > data[2] then
-      self._states[key] = nil
-      return false
-   end
-  
-	return data ~= nil
+function Debounce.IsActive(self: Types.ClassV2, key: string)
+	local data = self._states[key]
+	if not data then
+		return false
+	end
+	
+	if (os.clock() - data.time) > data.duration then
+		self._states[key] = nil
+		return false
+	end
+	
+	return true
 end
 
 -- Cancels a running debounce
-function Debounce.Skip(self: Types.Class, key: string)
-	local state = self._states[key]
+function Debounce.Skip(self: Types.ClassV2, key: string)
 	self._states[key] = nil
 end
 
 -- Cancels all debounces and clears the table.
-function Debounce.Clean(self: Types.Class)
-	for key, state in pairs(self._states) do
-		if state.timer then pcall(task.cancel, state.timer) end
-		self._states[key] = nil
-	end
+function Debounce.Clean(self: Types.ClassV2)
 	table.clear(self._states)
 end
 
