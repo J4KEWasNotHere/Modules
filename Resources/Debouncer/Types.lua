4ca4f9a36f3678b1d40a4cc9812a06137f5864fb
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-04-04 13:44:26 +0200

    Define ClassV2 for debounce with V2 support
    Added ClassV2 type definition for debounce functionality.

--- Resources/Debouncer/Types.lua
+++ Resources/Debouncer/Types.lua
@@ -15,4 +15,19 @@ export type Class = {
 	Destroy: (self: Class) -> (),
 }
 
+-- If your not using V2, remove this..
+
+type DebounceStateV2 = {number}
+
+export type ClassV2 = {
+	_states: {[string]: DebounceStateV2},
+	
+	Run: (self: Class, key: string, delayTime: number, fn: () -> ()) -> (boolean),
+	IsActive: (self: Class, key: string) -> (boolean),
+	
+	Skip: (self: Class, key: string) -> (),
+	
+	Destroy: (self: Class) -> (),
+}
+
 return {}
