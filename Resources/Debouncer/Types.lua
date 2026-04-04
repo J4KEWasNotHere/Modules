173a10bafc89db83363a3d91698532f5fa195574
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-04-04 13:53:27 +0200

    Refactor Class and ClassV2 types in Types.lua

--- Resources/Debouncer/Types.lua
+++ Resources/Debouncer/Types.lua
@@ -6,28 +6,31 @@ type DebounceState = {
 
 export type Class = {
 	_states: {[string]: DebounceState},
-	
+
 	Run: (self: Class, key: string, delayTime: number, fn: () -> ()) -> (boolean),
 	IsActive: (self: Class, key: string) -> (boolean),
-	
+
 	Skip: (self: Class, key: string) -> (),
-	
+
 	Destroy: (self: Class) -> (),
 }
 
 -- If your not using V2, remove this..
 
-type DebounceStateV2 = {number}
+type DebounceStateV2 = {
+	time: number,
+	duration: number,
+}
 
 export type ClassV2 = {
 	_states: {[string]: DebounceStateV2},
-	
-	Run: (self: Class, key: string, delayTime: number, fn: () -> ()) -> (boolean),
-	IsActive: (self: Class, key: string) -> (boolean),
-	
-	Skip: (self: Class, key: string) -> (),
-	
-	Destroy: (self: Class) -> (),
+
+	Run: (self: ClassV2, key: string, delayTime: number, fn: () -> ()) -> (boolean),
+	IsActive: (self: ClassV2, key: string) -> (boolean),
+
+	Skip: (self: ClassV2, key: string) -> (),
+
+	Destroy: (self: ClassV2) -> (),
 }
 
 return {}
