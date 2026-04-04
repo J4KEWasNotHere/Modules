177257dec205b61bb40b9f48c2a1036c3eec4be4
JakeyRoundHead <112113737+J4KEWasNotHere@users.noreply.github.com>
2026-04-04 13:41:45 +0200

    Add debounce functionality to DebouncerV2.lua
    Implement debounce functionality with methods for running, checking, skipping, and cleaning debounces.

--- Resources/Debouncer/DebouncerV2.lua
+++ Resources/Debouncer/DebouncerV2.lua
@@ -1,1 +1,60 @@
+--!strict
+--!optimize 2
 
+local Types = require("@self/Types")
+
+local Debounce = {}
+Debounce.__index = Debounce
+
+local Types = require(script.Types)
+
+function Debounce.new()
+	local self = {_states = {}}
+	return setmetatable(self, Debounce)
+end
+
+-- Runs a function if the key isn't in an active debounce
+function Debounce.Run(self: Types.Class, key: string, delayTime: number, fn: () -> ())
+	if self:IsActive(key) then
+    local data = self._states[key]
+    if (os.clock() - data[1]) > data[2] then
+      
+    else
+    	return false
+    end
+	end
+	
+	self._states[key] = {os.clock(), delayTime}
+
+	fn()
+	return true
+end
+
+-- Checks if a debounce is active for the given key
+function Debounce.IsActive(self: Types.Class, key: string)
+   local data = self._states[key]
+   
+   if (os.clock() - data[1]) > data[2] then
+      self._states[key] = nil
+      return false
+   end
+  
+	return data ~= nil
+end
+
+-- Cancels a running debounce
+function Debounce.Skip(self: Types.Class, key: string)
+	local state = self._states[key]
+	self._states[key] = nil
+end
+
+-- Cancels all debounces and clears the table.
+function Debounce.Clean(self: Types.Class)
+	for key, state in pairs(self._states) do
+		if state.timer then pcall(task.cancel, state.timer) end
+		self._states[key] = nil
+	end
+	table.clear(self._states)
+end
+
+return Debounce
